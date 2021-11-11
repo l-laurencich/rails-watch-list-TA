334f576dcdb1c9e5e80b00d6a7f8561a206a0d86
Lucas Laurencich <lucas.maximiliano89@gmail.com>
2021-11-11 12:21:11 +0100

    lunch is up

---
+++ db/migrate/20211111104744_add_comment_to_bookmarks.rb
@@ -0,0 +1,6 @@
+class AddCommentToBookmarks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :bookmarks, :comment, :text
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
